324219a3b558661bd919a18a25ccd3069af9b2e4
adeputraprimasuhendri <adeputra.mtech@gmail.com>
2021-02-17 17:15:40 +0700

    Setup FCM

---
+++ static/firebase-auth-sw.js
@@ -0,0 +1,31 @@
+importScripts('https://www.gstatic.com/firebasejs/8.2.7/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.7/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing the generated config
+var firebaseConfig = {
+    apiKey: "<apiKey>",
+    authDomain: "<authDomain>",
+    databaseURL: "<databaseURL>",
+    projectId: "<projectId>",
+    storageBucket: "<storageBucket>",
+    messagingSenderId: "<messagingSenderId>",
+    appId: "<appId>",
+    measurementId: "<measurementId>"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Received background message ', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
